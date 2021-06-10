260e892cc0c7e7b41928d7c0d82ebd991b55b2af
denis-torre <denis.torre@icahn.mssm.edu>
2021-06-10 14:21:17 -0400

    ChIP-Seq alignment

--- embryo-chipseq/pipeline/pipeline-embryo-chipseq.py
+++ embryo-chipseq/pipeline/pipeline-embryo-chipseq.py
@@ -104,6 +104,33 @@ def linkFASTQ(infile, outfile):
 	# Create
 	os.system('mkdir -p {outdir} && ln -s {infile} {outfile}'.format(**locals()))
 
+#############################################
+########## 2. Adapter trimming
+#############################################
+
+def trimJobs():
+	for sample_path in glob.glob('arion/chipseq/s01-fastq.dir/*/raw/*'):
+		infiles = glob.glob(os.path.join(sample_path, '*'))
+		infiles.sort()
+		outdir = sample_path.replace('/raw/', '/trimmed/')
+		yield [infiles, outdir]
+
+# @follows(linkFASTQ)
+
+@files(trimJobs)
+
+def trimIlluminaAdapters(infiles, outdir):
+
+	# Command
+	if len(infiles) == 1:
+		cmd_str = '''trim_galore --nextera --cores 6 --output_dir {outdir} {infiles[0]}'''.format(**locals())
+	elif len(infiles) == 2:
+		cmd_str = '''trim_galore --nextera --paired --cores 6 --output_dir {outdir} {infiles[0]} {infiles[1]}'''.format(**locals())
+
+	# Run
+	print(cmd_str)
+	# run_job(cmd_str, outdir, modules=['trim_galore/0.6.6'], W='02:00', GB=6, n=6, stdout=os.path.join(outdir, 'job.log'), stderr=os.path.join(outdir, 'job.err'))
+
 #######################################################
 #######################################################
 ########## S2. QC
@@ -126,7 +153,7 @@ def runFastQC(infile, outfile):
 	cmd_str = '''fastqc --outdir=$(dirname {outfile}) {infile}'''.format(**locals())
 	
 	# Run
-	run_job(cmd_str, outfile, modules=['fastqc/0.11.8'], W='03:00', GB=12, n=1, print_outfile=False)
+	run_job(cmd_str, outfile, modules=['fastqc/0.11.8'], W='03:00', GB=12, n=1, print_outfile=True)
 
 # ls arion/chipseq/s02-fastqc.dir/human/raw | wc -l
 
@@ -159,6 +186,126 @@ def runMultiQC(infile, outfile):
 	# Run
 	if not os.path.exists(outfile):
 		run_job(cmd_str, outfile, conda_env='env', W="01:00", GB=10, n=1, print_outfile=False, run_locally=False, stdout=outfile.replace('.html', '.log'))
+
+#######################################################
+#######################################################
+########## S3. Alignment
+#######################################################
+#######################################################
+
+#############################################
+########## 1. Bowtie
+#############################################
+
+# @follows(trimIlluminaAdapters, buildBowtieIndex)
+
+# @collate('arion/chipseq/s01-fastq.dir/human/trimmed/*/*_trimmed.fq.gz',
+@collate('arion/chipseq/s01-fastq.dir/human/raw/*/*.fastq.gz',
+ 		 regex(r'(.*)/s01-fastq.dir/(.*)/raw/(.*)/.*.fastq.gz'),
+		 add_inputs(r'arion/atacseq/s03-alignment.dir/\2/bowtie2/index/*primary_assembly.1.bt2'),
+		 r'\1/s03-alignment.dir/\2/bowtie2/results/\3/\3.bam')
+
+def runBowtie(infiles, outfile):
+
+	# Infiles
+	fastq = [x[0] for x in infiles]
+	bowtie_index = infiles[0][1].replace('.1.bt2', '')
+	outname = outfile.replace('.bam', '')
+
+	# FASTQ string
+	if len(fastq) == 1:
+		fastq_str = '-U {fastq[0]}'.format(**locals())
+	elif len(fastq) == 2:
+		fastq_str = '-1 {fastq[0]} -2 {fastq[1]}'.format(**locals())
+
+	# Command
+	cmd_str = '''bowtie2 -x {bowtie_index} {fastq_str} -q -p 40 | samtools view -bS --threads 40 | samtools sort --threads 40 -o {outfile} && \
+		samtools index -b {outfile} && samtools flagstat {outfile} > {outname}.flagstat && samtools idxstats {outfile} > {outname}.idxstats && samtools stats {outfile} > {outname}.stats && samtools view {outfile} | cut -f5 | sort | uniq -c | sort -b -k2,2n | sed -e 's/^[ \\t]*//' > {outname}.mapq'''.format(**locals())
+
+	# Run
+	run_job(cmd_str, outfile, W='03:00', n=10, GB=5, modules=['bowtie2/2.4.1', 'samtools/1.9'], stdout=outfile.replace('.bam', '.log'), stderr=outfile.replace('.bam', '.err'), print_cmd=False, ow=False)
+
+# find arion/chipseq/s03-alignment.dir/human/bowtie2/results -name "*.log" | js
+
+#############################################
+########## 2. Filter
+#############################################
+# Removes duplicates, and any reads not mapping to chromosomes 1-19, X, Y.
+# "[XS] == null" - multimappers
+# "not unmapped"
+# "not duplicate"
+# "ref_id <= 21 and ref_id != 19" - chromosomes 1-19,X,Y (no chrM)
+# "mapping_quality >= 30"
+# fragment lengths is empty for single end data
+
+# @transform(runBowtie,
+@transform('arion/chipseq/s03-alignment.dir/human/bowtie2/results/*/*0.bam',
+		   suffix('.bam'),
+		   '_filtered.bam')
+
+def filterBam(infile, outfile):
+
+	# Files
+	outname = outfile.replace('.bam', '')
+
+	# Command
+	cmd_str = ''' sambamba view --with-header --nthreads 30 --format bam --filter "ref_id <= 21 and ref_id != 19 and not unmapped and not duplicate and mapping_quality >= 30" {infile} > {outfile} && \
+		samtools index -b {outfile} && samtools flagstat {outfile} > {outname}.flagstat && samtools idxstats {outfile} > {outname}.idxstats && samtools stats {outfile} > {outname}.stats && samtools view {outfile} | awk '$9>0' | cut -f9 | sort | uniq -c | sort -b -k2,2n | sed -e 's/^[ \\t]*//' > {outname}.fragment_lengths.txt'''.format(**locals())
+
+	# Run
+	run_job(cmd_str, outfile, W='03:00', n=5, GB=5, modules=['samtools/1.9', 'sambamba/0.5.6'], print_outfile=True)
+
+#############################################
+########## 3. Create BigWig
+#############################################
+
+@transform(filterBam,
+		   suffix('.bam'),
+		   add_inputs('/sc/arion/projects/GuccioneLab/genome-indices/hg38/blacklists/hg38-blacklist.v2.bed'),
+		   '.bw')
+
+def createBigWig(infiles, outfile):
+
+	# Command
+	cmd_str = """bamCoverage --outFileFormat=bigwig --binSize=10 --skipNonCoveredRegions --numberOfProcessors=48 --normalizeUsing RPKM --blackListFileName {infiles[1]} -b {infiles[0]} -o {outfile}""".format(**locals())
+
+	# Run
+	run_job(cmd_str, outfile, conda_env='env', W='06:00', n=8, GB=4, print_outfile=False)
+
+#######################################################
+#######################################################
+########## S4. Peaks
+#######################################################
+#######################################################
+
+#############################################
+########## 1. MACS2
+#############################################
+
+# @collate(filterBam,
+# 		 regex(r'(.*)/s03-alignment.dir/(.*)/bowtie2/results/(.*)_Rep.*/.*_filtered.bam'),
+# 		 r'\1/s04-peaks.dir/\2/macs2/\3/\3_peaks.xls')
+
+# def runMacs2(infiles, outfile):
+
+# 	# Infiles
+# 	infiles_str = ' '.join(infiles)
+# 	basename = outfile.replace('_peaks.xls', '')
+# 	organism = outfile.split('/')[-4].replace('human', 'hs').replace('mouse', 'mm')
+
+# 	# Command
+# 	cmd_str = ''' macs2 callpeak \
+# 		-t {infiles_str}\
+# 		-f BAM \
+# 		--nomodel \
+# 		--nolambda \
+# 		--extsize 250 \
+# 		-q 0.01 \
+# 		-n {basename} \
+# 		-g {organism}'''.format(**locals())
+
+# 	# Run
+# 	run_job(cmd_str, outfile, modules=['macs/2.1.0'], W='06:00', n=1, GB=30, print_cmd=False, stdout=outfile.replace('.xls', '.log'), stderr=outfile.replace('.xls', '.err'))
 
 #######################################################
 #######################################################
